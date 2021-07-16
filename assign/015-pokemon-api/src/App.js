89426d2719434fcca426d507d591f243094b6705
Garrett Schmahl <gschmahl@pm.me>
2021-07-16 09:55:36 -0700

    pokemon api

---
+++ assign/015-pokemon-api/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1118")
+      .then((response) => response.json())
+      .then((response) => setPokemon(response.results))
+      .catch((err) => {
+        console.log(err);
+      }, []);
+  });
+
+  return (
+    <div className="App">
+      {pokemon.length > 0 &&
+        pokemon.map((pokemon, index) => {
+          return <div key={index}>{pokemon.name}</div>;
+        })}
+    </div>
+  );
+}
+
+export default App;
